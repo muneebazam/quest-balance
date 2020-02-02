5485dd8a518fdb59dabe504eb53f2dee3ee71e80
Muneeb Azam <42183127+muneebazam@users.noreply.github.com>
2020-02-02 14:05:33 -0500

    Update quest_balance_calculator.py

--- quest_balance_calculator.py
+++ quest_balance_calculator.py
@@ -16,9 +16,9 @@ def total_balances(margin_balance, tfsa_balance):
 
     # Construct balance message
     balances = []
-    balances.append("Total Equity: _${}_\n".format(equity))
-    balances.append("Market Value: _${}_\n".format(marketValue))
-    balances.append("Cash: _${}_\n".format(cash))
+    balances.append(">Total Equity: _${}_\n".format(equity))
+    balances.append(">Market Value: _${}_\n".format(marketValue))
+    balances.append(">Cash: _${}_\n".format(cash))
     return ''.join(balances)
 
 # Calculates balance information for specific account
@@ -26,9 +26,9 @@ def calculate_balances(balances):
 
     combinedBalances = balances["combinedBalances"][0]
     balances = []
-    balances.append("Total Equity: _${}_\n".format(round(combinedBalances["totalEquity"], 2)))
-    balances.append("Market Value: _${}_\n".format(round(combinedBalances["marketValue"], 2)))
-    balances.append("Cash: _${}_\n".format(round(combinedBalances["cash"], 2)))
+    balances.append(">Total Equity: _${}_\n".format(round(combinedBalances["totalEquity"], 2)))
+    balances.append(">Market Value: _${}_\n".format(round(combinedBalances["marketValue"], 2)))
+    balances.append(">Cash: _${}_\n".format(round(combinedBalances["cash"], 2)))
     return ''.join(balances)
 
 # Calculates position information for specific account
@@ -44,7 +44,7 @@ def calculate_positions(positions):
 
         # Add Open P&L value to message
         symbol = position["symbol"]
-        position_info.append("{}: ".format(symbol))
+        position_info.append(">{}: ".format(symbol))
         if (openPnl > 0):
             position_info.append("+")
         position_info.append("_{}_  ".format(openPnl))
